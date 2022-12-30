2e069e00bc06ca6c874dc916486367612fd5c5a0
hiperDG <104842093+ddantas42@users.noreply.github.com>
2022-12-30 15:10:35 +0000

    Add files via upload

---
+++ src/key_events.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   key_events.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ddantas- <ddantas-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/15 17:37:20 by ddantas-          #+#    #+#             */
+/*   Updated: 2022/12/30 13:45:11 by ddantas-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../so_long.h"
+
+int	keypress_actions(int keycode, t_vars *vars)
+{
+	ft_printf("Key pressed = %d\n", keycode);
+	if (keycode == ESCAPE)
+		mlx_destroy_window(vars->mlx, vars->mlx_win);
+	return (0);
+}
